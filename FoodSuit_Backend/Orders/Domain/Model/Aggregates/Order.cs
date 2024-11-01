f4971fde921bdc96cd15e0af214063693731525b
P1ero0VL <u20211a620@upc.edu.pe>
2024-11-01 18:35:02 -0500

    feat(orders): add aggregates, commands and queries

---
+++ FoodSuit_Backend/Orders/Domain/Model/Aggregates/Order.cs
@@ -0,0 +1,36 @@
+﻿using FoodSuit_Backend.Orders.Domain.Model.Commands;
+
+namespace FoodSuit_Backend.Orders.Domain.Model.Aggregates;
+
+public partial class Order
+{
+    
+    public int Id { get; }
+    public DateTime Date { get; private set; }
+    public string Table { get; private set; }
+    public string Status { get; private set; }
+    //public OrderDishes Dishes { get; private set; }
+    //public WaiterName Name { get; private set; }
+    public string Total { get; private set; }
+    
+    //public string FullName => Name.FullName;
+    
+
+    protected Order()
+    {
+        Table = string.Empty;
+        Status = string.Empty;
+        //Dishes = new OrderDishes();
+        //Name = new WaiterName();
+        Total = string.Empty;
+        Date = DateTime.Now;
+    }
+    
+    public Order(CreateOrderCommand command)
+    {
+        Table = command.Table;
+        Status = command.Status;
+        Total = command.Total;
+        Date = DateTime.Now;
+    }
+}
