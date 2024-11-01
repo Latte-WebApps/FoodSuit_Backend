a59f6c8377460a9ac04d5d751513784efddb254a
JosuePaiva02 <u202119095@upc.edu.pe>
2024-11-01 18:18:19 -0500

    feat: implemented services for commands and queries for employees

---
+++ FoodSuit_Backend/Employees/Application/Internal/CommandServices/EmployeeCommandService.cs
@@ -0,0 +1,12 @@
+﻿using FoodSuit_Backend.Employees.Domain.Model.Aggregates;
+using FoodSuit_Backend.Employees.Domain.Model.Commands;
+using FoodSuit_Backend.Employees.Domain.Repositories;
+using FoodSuit_Backend.Employees.Domain.Services;
+using FoodSuit_Backend.Shared.Domain.Repositories;
+
+namespace FoodSuit_Backend.Employees.Application.Internal.CommandServices;
+
+public class EmployeeCommandService(IEmployeeRepository employeeRepository, IUnitOfWork unitOfWork) : IEmployeeCommandService
+{
+    
+}
---
+++ FoodSuit_Backend/Employees/Application/Internal/QueryServices/EmployeeQueryService.cs
@@ -0,0 +1,8 @@
+﻿using FoodSuit_Backend.Shared.Domain.Repositories;
+
+namespace FoodSuit_Backend.Employees.Application.Internal.QueryServices;
+
+public class EmployeeQueryService
+{
+    
+}
