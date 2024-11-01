6f2381222cd7810eed96bc5a9dbb19d27fa9f36e
JosuePaiva02 <u202119095@upc.edu.pe>
2024-11-01 18:19:01 -0500

    feat:  added interfaces for Employee services

---
+++ FoodSuit_Backend/Employees/Domain/Services/IEmployeeCommandService.cs
@@ -0,0 +1,9 @@
+﻿using FoodSuit_Backend.Employees.Domain.Model.Aggregates;
+using FoodSuit_Backend.Employees.Domain.Model.Commands;
+
+namespace FoodSuit_Backend.Employees.Domain.Services;
+
+public interface IEmployeeCommandService
+{
+    Task<Employee?> Handle(CreateEmployeeCommand command);
+}
