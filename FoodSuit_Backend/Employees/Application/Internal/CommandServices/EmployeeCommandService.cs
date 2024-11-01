0bc0782eafdd49ff016c27dfab7c5cc19e0c9b70
JosuePaiva02 <u202119095@upc.edu.pe>
2024-11-01 18:29:16 -0500

    feat: implemente command services for Employees

--- FoodSuit_Backend/Employees/Application/Internal/CommandServices/EmployeeCommandService.cs
+++ FoodSuit_Backend/Employees/Application/Internal/CommandServices/EmployeeCommandService.cs
@@ -8,5 +8,18 @@ namespace FoodSuit_Backend.Employees.Application.Internal.CommandServices;
 
 public class EmployeeCommandService(IEmployeeRepository employeeRepository, IUnitOfWork unitOfWork) : IEmployeeCommandService
 {
-    
+    public async Task<Employee?> Handle(CreateEmployeeCommand command)
+    {
+        var employee = new Employee(command);
+        try
+        {
+            await employeeRepository.AddAsync(employee);
+            await unitOfWork.CompleteAsync();
+            return employee;
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
 }
