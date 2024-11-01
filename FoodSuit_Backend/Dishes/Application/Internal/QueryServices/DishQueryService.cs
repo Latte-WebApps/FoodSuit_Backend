945dbc627e2856f1948638c459da925d92ba1f6d
P1ero0VL <u20211a620@upc.edu.pe>
2024-11-01 17:10:32 -0500

    feat(dish): add query service implementation

---
+++ FoodSuit_Backend/Dishes/Application/Internal/QueryServices/DishQueryService.cs
@@ -0,0 +1,20 @@
+﻿using FoodSuit_Backend.Dishes.Domain.Model.Aggregates;
+using FoodSuit_Backend.Dishes.Domain.Model.Queries;
+using FoodSuit_Backend.Dishes.Domain.Repositories;
+using FoodSuit_Backend.Dishes.Domain.Services;
+
+namespace FoodSuit_Backend.Dishes.Application.Internal.QueryServices;
+
+public class DishQueryService(IDishRepository dishRepository) : IDishQueryService
+{
+    public async Task<Dish?> Handle(GetDishByIdQuery query)
+    {
+        return await dishRepository.FindDishByIdAsync(query.Id);
+       
+    }
+
+    public async Task<IEnumerable<Dish>> Handle(GetAllDishesByCategoryQuery query)
+    {
+        return await dishRepository.FindByCategoryAsync(query.Category);
+    }
+}
