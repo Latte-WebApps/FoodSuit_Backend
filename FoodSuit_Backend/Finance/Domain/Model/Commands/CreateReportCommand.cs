4968a382dc29051203bbeabc2c0c5a141920935c
pachieeee <sebaspacheco2003@gmail.com>
2024-11-02 10:43:08 -0500

    feat(commands): added extra variables to match with new report entity.

--- FoodSuit_Backend/Finance/Domain/Model/Commands/CreateReportCommand.cs
+++ FoodSuit_Backend/Finance/Domain/Model/Commands/CreateReportCommand.cs
@@ -2,4 +2,4 @@ using FoodSuit_Backend.Finance.Domain.Model.ValueObjects;
 
 namespace FoodSuit_Backend.Finance.Domain.Model.Commands;
 
-public record CreateReportCommand(string Description, EReportType ReportType);
+public record CreateReportCommand(string Description, EReportType ReportType, string Date, int Amount, int OrdersId, int ProductsId);
