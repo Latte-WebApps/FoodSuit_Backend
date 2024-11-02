6fb12142d5bc11921bff2db58a9f2dc364d26b72
JeanGrandez <jpmle10@hotmail.com>
2024-11-02 14:40:13 -0500

    fix: attendance repository references

--- FoodSuit_Backend/Attendance/Domain/Repositories/IAttendanceRepository.cs
+++ FoodSuit_Backend/Attendance/Domain/Repositories/IAttendanceRepository.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using FoodSuit_Backend.Attendance.Domain.Model.Aggregates;
 using FoodSuit_Backend.Shared.Domain.Repositories;
 
 namespace FoodSuit_Backend.Attendance.Domain.Repositories;
@@ -6,10 +6,9 @@ namespace FoodSuit_Backend.Attendance.Domain.Repositories;
 /// <summary>
 /// Interface for Attendance Repository
 /// </summary>
-public interface IAttendanceRepository : IBaseRepository<Attendance>
+public interface IAttendanceRepository : IBaseRepository<EmployeeAttendance>
 {
-    Task<IEnumerable<Attendance>> FindByEmployeeIdAsync(int employeeId, DateTime? startDate = null, DateTime? endDate = null);
-    
-    Task<IEnumerable<Attendance>> FindByDateAsync(DateTime date);
-    Task<Attendance?> GetByIdAsync(int id);
+    Task<IEnumerable<EmployeeAttendance>> FindByEmployeeIdAsync(int employeeId, DateTime? startDate = null, DateTime? endDate = null);
+    Task<IEnumerable<EmployeeAttendance>> FindByDateAsync(DateTime date);
+    Task<EmployeeAttendance?> GetByIdAsync(int id);
 }
