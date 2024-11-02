0bafe820d140436162826807c27f09d8f76afd32
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-11-02 16:42:53 -0500

    feat(attendance): added builders for db

--- FoodSuit_Backend/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ FoodSuit_Backend/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using FoodSuit_Backend.Attendance.Domain.Model.Aggregates;
 using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,18 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        
+        builder.Entity<EmployeeAttendance>().HasKey(f => f.Id);
+        builder.Entity<EmployeeAttendance>().Property(f => f.Id)
+            .IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<EmployeeAttendance>().Property(f => f.EmployeeId)
+            .IsRequired();
+        builder.Entity<EmployeeAttendance>().Property(f => f.Date)
+            .IsRequired();
+        builder.Entity<EmployeeAttendance>().Property(f => f.CheckInTime)
+            .IsRequired();
+        builder.Entity<EmployeeAttendance>().Property(f => f.CheckOutTime);
+        
         
         builder.UseSnakeCaseNamingConvention();
 
