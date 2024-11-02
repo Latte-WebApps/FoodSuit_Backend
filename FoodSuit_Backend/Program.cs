a58efd3dd9058eb585b6d628bef710bffdf5176f
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-11-02 04:48:12 -0500

    feat(Inventory): set builders for Inventory

--- FoodSuit_Backend/Program.cs
+++ FoodSuit_Backend/Program.cs
@@ -1,13 +1,65 @@
-var builder = WebApplication.CreateBuilder(args);
+using FoodSuit_Backend.Inventory.Application.Internal.CommandServices;
+using FoodSuit_Backend.Inventory.Application.Internal.QueryServices;
+using FoodSuit_Backend.Inventory.Domain.exceptions;
+using FoodSuit_Backend.Inventory.Domain.Repositories;
+using FoodSuit_Backend.Inventory.Domain.Services;
+using FoodSuit_Backend.Inventory.Infrastructure.Persistence.EFC.Repositories;
+using FoodSuit_Backend.Shared.Domain.Repositories;
+using FoodSuit_Backend.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (connectionString is null)
+    throw new Exception("Database connection string is not set.");
+
+if (builder.Environment.IsDevelopment())
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    });
+else if (builder.Environment.IsProduction())
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseMySQL(connectionString);
+    });
+
+// Configure Dependency Injection
+
+// Shared Bounded Context Dependency Injection Configuration
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// News Bounded Context Dependency Injection Configuration
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductCommandService, ProductCommandService>();
+builder.Services.AddScoped<IProductQueryService, ProductQueryService>();
 
 var app = builder.Build();
+
+// app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
