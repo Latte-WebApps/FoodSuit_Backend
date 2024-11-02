177ca57b358eaafeb4db6f5f9e093e7e784c380c
JosuePaiva02 <u202119095@upc.edu.pe>
2024-11-02 13:25:37 -0500

    fix:  deleted double method in BaseRepository

--- FoodSuit_Backend/Shared/Domain/Repositories/IBaseRepository.cs
+++ FoodSuit_Backend/Shared/Domain/Repositories/IBaseRepository.cs
@@ -7,6 +7,4 @@ public interface IBaseRepository<TEntity>
     void Update(TEntity entity);
     void Remove(TEntity entity);
     Task<IEnumerable<TEntity>> ListAsync();
-    Task<TEntity?> FindByIdAsync(int id);
-
 }
