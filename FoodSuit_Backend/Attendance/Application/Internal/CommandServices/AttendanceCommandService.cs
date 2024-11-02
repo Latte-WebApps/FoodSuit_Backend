93cc7b4b74d8851a2b99dc8fc214066e0b9e6852
JeanGrandez <jpmle10@hotmail.com>
2024-11-02 14:43:18 -0500

    feat(attendance): added application command and queries services

---
+++ FoodSuit_Backend/Attendance/Application/Internal/CommandServices/AttendanceCommandService.cs
@@ -0,0 +1,72 @@
+﻿using FoodSuit_Backend.Attendance.Domain.Services;
+using FoodSuit_Backend.Attendance.Domain.Model.Aggregates;
+using FoodSuit_Backend.Attendance.Domain.Model.Commands;
+using FoodSuit_Backend.Attendance.Domain.Repositories;
+using FoodSuit_Backend.Shared.Domain.Repositories;
+using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace FoodSuit_Backend.Attendance.Application.Internal.CommandServices;
+
+public class AttendanceCommandService(IAttendanceRepository attendanceRepository, IUnitOfWork unitOfWork) 
+    : IAttendanceCommandService
+{
+    public async Task<EmployeeAttendance?> Handle(RegisterAttendanceCommand command)
+    {
+        var attendance = new EmployeeAttendance(command);
+
+        try
+        {
+            await attendanceRepository.AddAsync(attendance);
+            await unitOfWork.CompleteAsync();
+            return attendance;
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+
+    public async Task<EmployeeAttendance?> Handle(int id, UpdateCheckOutCommand command)
+    {
+        var attendance = await attendanceRepository.GetByIdAsync(id);
+
+        if (attendance == null)
+        {
+            return null;
+        }
+
+        attendance.UpdateCheckOutTime(command.CheckOutTime);
+
+        try
+        {
+            await unitOfWork.UpdateAsync(attendance);
+            await unitOfWork.CompleteAsync();
+            return attendance;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error updating attendance", ex);
+        }
+    }
+
+    public async Task<bool> Handle(DeleteAttendanceCommand command)
+    {
+        var attendance = await attendanceRepository.GetByIdAsync(command.AttendanceId);
+
+        if (attendance == null)
+        {
+            return false;
+        }
+
+        try
+        {
+            await unitOfWork.RemoveAsync(attendance);
+            await unitOfWork.CompleteAsync();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}
---
+++ FoodSuit_Backend/Attendance/Application/Internal/QueryServices/AttendanceQueryService.cs
@@ -0,0 +1,26 @@
+﻿using FoodSuit_Backend.Attendance.Domain.Model.Aggregates;
+using FoodSuit_Backend.Attendance.Domain.Model.Queries;
+using FoodSuit_Backend.Attendance.Domain.Repositories;
+using FoodSuit_Backend.Attendance.Domain.Services;
+
+namespace FoodSuit_Backend.Attendance.Application.Internal.QueryServices;
+
+public class AttendanceQueryService(IAttendanceRepository attendanceRepository) 
+    : IAttendanceQueryService
+{
+    public async Task<IEnumerable<EmployeeAttendance>> Handle(GetAttendanceByEmployeeIdQuery query)
+    {
+        return await attendanceRepository.FindByEmployeeIdAsync(query.EmployeeId, query.StartDate, query.EndDate);
+    }
+
+    public async Task<IEnumerable<EmployeeAttendance>> Handle(GetDailyAttendanceByDateQuery query)
+    {
+        return await attendanceRepository.FindByDateAsync(query.Date);
+    }
+
+    public async Task<double> Handle(GetTotalHoursWorkedByEmployeeIdQuery query)
+    {
+        var attendances = await attendanceRepository.FindByEmployeeIdAsync(query.EmployeeId, query.StartDate, query.EndDate);
+        return attendances.Sum(a => a.CalculateHoursWorked());
+    }
+}
