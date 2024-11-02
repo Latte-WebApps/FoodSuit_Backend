6ca768973e120776313fb18b0a7baa08ab45478e
JeanGrandez <jpmle10@hotmail.com>
2024-11-02 14:42:12 -0500

    feat(attendance): add interfaces

---
+++ FoodSuit_Backend/Attendance/Interfaces/ACL/IAttendanceContextFacade.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodSuit_Backend.Attendance.Interfaces.ACL;
+
+public interface IAttendanceContextFacade
+{
+    Task<int> RegisterAttendance(int employeeId, DateTime date, DateTime checkInTime);
+    Task<bool> UpdateCheckOut(int attendanceId, DateTime checkOutTime);
+    Task<bool> DeleteAttendance(int attendanceId);
+}
