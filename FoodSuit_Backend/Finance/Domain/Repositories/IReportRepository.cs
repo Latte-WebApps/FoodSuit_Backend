60ffbbaf1bf02f2ee1f07f6d31cb26f2557f9be3
pachieeee <sebaspacheco2003@gmail.com>
2024-11-02 04:34:10 -0500

    feat(repositories): added report repository interface.

---
+++ FoodSuit_Backend/Finance/Domain/Repositories/IReportRepository.cs
@@ -0,0 +1,9 @@
+using FoodSuit_Backend.Finance.Domain.Model.Entities;
+using FoodSuit_Backend.Shared.Domain.Repositories;
+
+namespace FoodSuit_Backend.Finance.Domain.Repositories;
+
+public interface IReportRepository : IBaseRepository<Report>
+{
+    
+}
