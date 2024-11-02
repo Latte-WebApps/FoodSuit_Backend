52fcc6f56cb8375454b7c549af61e90a719861d3
pachieeee <sebaspacheco2003@gmail.com>
2024-11-02 10:45:15 -0500

    feat(configuration): added finance context to AppDbContext.

--- FoodSuit_Backend/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ FoodSuit_Backend/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using FoodSuit_Backend.Finance.Domain.Model.Entities;
 using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,22 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        
+        // Finance Context
+        builder.Entity<Report>().HasKey(r => r.Id);
+        builder.Entity<Report>().Property(r => r.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Report>().Property(r => r.Description).IsRequired().HasMaxLength(100);
+        builder.Entity<Report>().HasDiscriminator<string>("report_type")
+            .HasValue<Report>("Expense")
+            .HasValue<Report>("Earning");
+        builder.Entity<Report>().Property(r => r.Date).IsRequired();
+        builder.Entity<Report>().Property(r => r.Amount).IsRequired();
+        
+        // Replace with actual foreign keys
+        builder.Entity<Report>().Property(r => r.OrdersId).IsRequired();
+        builder.Entity<Report>().Property(r => r.ProductsId).IsRequired();
+        
+        // TODO: Add relationships between tables
         
         builder.UseSnakeCaseNamingConvention();
 
