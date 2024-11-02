5d07facaa46de333b8ec7af6299558c7f4702749
JeanGrandez <jpmle10@hotmail.com>
2024-11-02 14:40:54 -0500

    feat(attendance): add services

---
+++ FoodSuit_Backend/Attendance/Domain/Services/IAttendanceQueryService.cs
@@ -0,0 +1,11 @@
+﻿using FoodSuit_Backend.Attendance.Domain.Model.Aggregates;
+using FoodSuit_Backend.Attendance.Domain.Model.Queries;
+
+namespace FoodSuit_Backend.Attendance.Domain.Services;
+
+public interface IAttendanceQueryService
+{
+    Task<IEnumerable<EmployeeAttendance>> Handle(GetAttendanceByEmployeeIdQuery query);
+    Task<IEnumerable<EmployeeAttendance>> Handle(GetDailyAttendanceByDateQuery query);
+    Task<double> Handle(GetTotalHoursWorkedByEmployeeIdQuery query);
+}
