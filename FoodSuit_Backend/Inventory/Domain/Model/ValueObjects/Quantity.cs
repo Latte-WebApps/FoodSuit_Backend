566593f6b1e4dcb3630c0740b8ee9853d09560fd
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-11-02 04:41:22 -0500

    feat(Inventory): add aggregate and value object

---
+++ FoodSuit_Backend/Inventory/Domain/Model/ValueObjects/Quantity.cs
@@ -0,0 +1,16 @@
+﻿namespace FoodSuit_Backend.Inventory.Domain.Model.ValueObjects;
+
+public class Quantity
+{
+    public int Value { get; private set; }
+
+    public Quantity(int value)
+    {
+        if (value < 0)
+            throw new ArgumentException("Quantity cannot be negative.");
+        Value = value;
+    }
+
+    // Necesario para EF Core
+    private Quantity() { }
+}
