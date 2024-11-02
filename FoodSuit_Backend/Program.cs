1b42380550bfd13b61991140a882929bcd964165
JosuePaiva02 <u202119095@upc.edu.pe>
2024-11-02 13:17:32 -0500

    fix:  fixed program.cs

--- FoodSuit_Backend/Program.cs
+++ FoodSuit_Backend/Program.cs
@@ -1,4 +1,3 @@
-
 using FoodSuit_Backend.Employees.Application.Internal.CommandServices;
 using FoodSuit_Backend.Employees.Application.Internal.QueryServices;
 using FoodSuit_Backend.Employees.Domain.Repositories;
@@ -9,6 +8,27 @@ using FoodSuit_Backend.Shared.Infrastructure.Interfaces.ASP.Configuration;
 using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Configuration;
 using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Repositories;
 using Microsoft.EntityFrameworkCore;
+
+using FoodSuit_Backend.Inventory.Application.Internal.CommandServices;
+using FoodSuit_Backend.Inventory.Application.Internal.QueryServices;
+using FoodSuit_Backend.Inventory.Domain.exceptions;
+using FoodSuit_Backend.Inventory.Domain.Repositories;
+using FoodSuit_Backend.Inventory.Domain.Services;
+using FoodSuit_Backend.Inventory.Infrastructure.Persistence.EFC.Repositories;
+
+using FoodSuit_Backend.Finance.Application.Internal.CommandServices;
+using FoodSuit_Backend.Finance.Application.Internal.QueryServices;
+using FoodSuit_Backend.Finance.Domain.Repositories;
+using FoodSuit_Backend.Finance.Domain.Services;
+using FoodSuit_Backend.Finance.Infrastructure.Persistence.EFC.Repositories;
+
+using FoodSuit_Backend.Shared.Domain.Repositories;
+using FoodSuit_Backend.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -52,7 +72,17 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IEmployeeCommandService, EmployeeCommandService>();
 builder.Services.AddScoped<IEmployeeQueryService, EmployeeQueryService>();
+// Finance Bounded Context Dependency Injection Configuration
 
+builder.Services.AddScoped<IReportRepository, ReportRepository>();
+builder.Services.AddScoped<IReportCommandService, ReportCommandService>();
+builder.Services.AddScoped<IReportQueryService, ReportQueryService>();
+
+// Inventory Bounded Context Dependency Injection Configuration
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductCommandService, ProductCommandService>();
+builder.Services.AddScoped<IProductQueryService, ProductQueryService>();
 var app = builder.Build();
 
 // Verify Database Objects are Created
