4014b56075c528416eeba619adb030d7d017c353
pachieeee <sebaspacheco2003@gmail.com>
2024-11-02 10:42:49 -0500

    feat(resources): added extra variables to match with new report entity.

--- FoodSuit_Backend/Finance/Interfaces/REST/Resources/CreateReportResource.cs
+++ FoodSuit_Backend/Finance/Interfaces/REST/Resources/CreateReportResource.cs
@@ -1,3 +1,3 @@
 namespace FoodSuit_Backend.Finance.Interfaces.REST.Resources;
 
-public record CreateReportResource(string Description, string ReportType);
+public record CreateReportResource(string Description, string ReportType, string Date, int Amount, int OrdersId, int ProductsId);
