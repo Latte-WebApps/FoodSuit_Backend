9ea65d504007fbc9ccf2cb12ba81a4fa0d6cb15a
P1er0VL <u20211a620@upc.edu.pe>
2024-11-02 13:13:26 -0500

    feat(order): add orders bounded context

--- FoodSuit_Backend/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ FoodSuit_Backend/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using FoodSuit_Backend.Dishes.Domain.Model.Aggregates;
+using FoodSuit_Backend.Orders.Domain.Model.Aggregates;
 using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,11 +13,32 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
         builder.AddCreatedUpdatedInterceptor();
         base.OnConfiguring(builder);
     }
-
-
+    
+    
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        
+        //Orders Context
+        
+        builder.Entity<Dish>().ToTable("Dishes");
+        builder.Entity<Dish>().HasKey(d => d.Id);
+        builder.Entity<Dish>().Property(d => d.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Dish>().Property(d => d.Name).IsRequired();
+        builder.Entity<Dish>().Property(d=>d.Category).IsRequired();
+        builder.Entity<Dish>().Property(d=>d.Price).IsRequired();
+        
+        
+        builder.Entity<Order>().ToTable("Orders");
+        builder.Entity<Order>().HasKey(o => o.Id);
+        builder.Entity<Order>().Property(o => o.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Order>().Property(o=>o.Status).IsRequired();
+        builder.Entity<Order>().Property(o=>o.Date).IsRequired();
+        builder.Entity<Order>().Property(o=>o.Table).IsRequired();
+        
+        
+        
+
         
         builder.UseSnakeCaseNamingConvention();
 
