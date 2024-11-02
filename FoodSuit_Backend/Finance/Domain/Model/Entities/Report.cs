02405f3599eb42c4053f2ba35b46a69b1a8d1f93
pachieeee <sebaspacheco2003@gmail.com>
2024-11-02 10:37:05 -0500

    fix(entities): added additional variables to entity.

--- FoodSuit_Backend/Finance/Domain/Model/Entities/Report.cs
+++ FoodSuit_Backend/Finance/Domain/Model/Entities/Report.cs
@@ -7,10 +7,16 @@ public class Report
     public int Id { get; set; }
     public string Description { get; set; }
     public EReportType Type { get; private set; }
+    public string Date { get; set;  }
+    public int OrdersId { get; set; }
+    public int ProductsId { get; set; }
     
     public Report(EReportType type)
     {
         Description = string.Empty;
         Type = type;
+        Date = string.Empty;
+        OrdersId = 0;
+        ProductsId = 0;
     }
 }
