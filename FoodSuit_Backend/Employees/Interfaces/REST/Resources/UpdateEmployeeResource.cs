b979a878dbc92b6cb2f1b60c3c670974b2993a84
JosuePaiva02 <u202119095@upc.edu.pe>
2024-11-13 22:09:16 -0500

    fix:  namespaces

--- FoodSuit_Backend/Employees/Interfaces/REST/Resources/UpdateEmployeeResource.cs
+++ FoodSuit_Backend/Employees/Interfaces/REST/Resources/UpdateEmployeeResource.cs
@@ -1,3 +1,3 @@
-﻿namespace FoodSuit_Backend.Employees.Interfaces.REST.Transform;
+﻿namespace FoodSuit_Backend.Employees.Interfaces.REST.Resources;
 
 public record UpdateEmployeeResource(string FirstName, string LastName, int EntryHour, int EntryMinute, int ExitHour, int ExitMinute);
