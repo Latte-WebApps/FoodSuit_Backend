ee9e702550ca85015a52c45960ebd3437aa7b69d
P1ero0VL <u20211a620@upc.edu.pe>
2024-11-20 01:36:43 -0500

    feat(inventory): added inventory bounded context

---
+++ FoodSuit_Backend/Dishes/Application/Internal/OutboundServices/ACL/ExternalProductService.cs
@@ -0,0 +1,19 @@
+﻿namespace FoodSuit_Backend.Dishes.Application.Internal.OutboundServices.ACL;
+
+public class ExternalProductService
+{
+    public async Task<ProductId?> FetchProductIdByName(string name)
+    {
+        // Fetch product id from external service
+        var productId = await productsContextFacade.FetchProductIdByName(name);
+        if (productId == 0) return await Task.FromResult<ProductId?>(null);
+        return new ProductId(productId);
+    }
+
+    public async Task<ProductId?> CreateProduct(string name)
+    {
+        var productId = await productsContextFacade.CreateProduct(name);
+        if (productId == 0) return await Task.FromResult<ProductId?>(null);
+        return new ProductId(productId);
+    }
+}
---
+++ FoodSuit_Backend/Dishes/Domain/Model/Entities/Products.cs
@@ -0,0 +1,15 @@
+﻿namespace FoodSuit_Backend.Dishes.Domain.Model.Entities;
+
+public class Products
+{
+    public int Id { get; set; }
+    public string Name { get; private set; }
+    public int Quantity { get; private set; }
+
+    public Products(int id, string name, int quantity)
+    {
+        Id = id;
+        Name = name;
+        Quantity = quantity;
+    }
+}
